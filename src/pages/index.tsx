f2eaeae37e72b56cff25bf320a372c7fc7bb8174
Geazi Oliveira da Fonseca <geazioliveira@cefetmg.br>
2023-04-24 19:52:36 -0300

    implementado checkout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image"
-import Link from "next/link"
 import {useKeenSlider} from "keen-slider/react"
 import { HomeContainer, Product } from "@/styles/pages/home"
 
 
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe"
-import { GetServerSideProps, GetStaticProps } from "next"
+import {  GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps{
@@ -14,7 +13,7 @@ interface HomeProps{
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -31,15 +30,15 @@ export default function Home({products}:HomeProps) {
   <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return(
-          <Link key={product.id}href={`/product/${product.id}`} >
-          <Product  className="keen-slider__slide">
+
+          <Product key={product.id} href={`/product/${product.id}`} className="keen-slider__slide" prefetch={false}>
             <Image src={product.imageUrl} width={520} height={480} alt={""}/>
+
             <footer>
               <strong>{product.name}</strong>
               <span>{product.price}</span>
             </footer>
           </Product>
-        </Link>
         )
       })}
     </HomeContainer>
